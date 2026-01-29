b53f17b786ff9e50da93f18df6aec8a0d978dde6
Lina F <buh_19@inbox.ru>
2026-01-30 01:21:00 +0300

    Добавила проверку на формат ответа (словарь) в последнем тесте

--- tests/test_store.py
+++ tests/test_store.py
@@ -83,5 +83,6 @@ class TestStore:
         with allure.step("Проверка статуса ответа и валидация формата ответа"):
             assert response.status_code == 200, "Код ответа не совпал с ожидаемым "
             jsonschema.validate(response_json, STORE_SCHEMA)
+            assert isinstance(response_json, dict)
 
 
